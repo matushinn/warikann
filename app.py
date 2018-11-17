7e311c9f1dbac7fbc77beff4b6b2805b1f7e0493
shotaro-ooe <shoooe97@gmail.com>
2018-11-17 17:44:27 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,23 @@
+total_price = int(input("合計金額を教えてね(円):"))
+number_of_people = int(input("人数を教えてね(人):"))
+
+print(total_price)
+
+# print(type(total_price))
+# print(type((number_of_people)))
+
+# print(total_price/number_of_people)
+
+print(f"1人あたり: {total_price // number_of_people}円, 端数: {total_price % number_of_people}円")
+
+
+
+
+
+
+
+
+
+
+
+
